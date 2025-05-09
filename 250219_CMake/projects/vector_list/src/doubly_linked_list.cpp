16d81364dd7fb60e83a139a050013ed68d84e372
Val28He <bogdanovavalentina0405@gmail.com>
2025-05-09 14:14:49 +0300

    fixing errors vector_list

--- 250219_CMake/projects/vector_list/src/doubly_linked_list.cpp
+++ 250219_CMake/projects/vector_list/src/doubly_linked_list.cpp
@@ -7,7 +7,7 @@ using biv::DoublyLinkedList;
 template<typename T>
 DoublyLinkedList<T>::~DoublyLinkedList() {
 	Node* ptr = begin;
-	while (ptr != NULL){
+	while (ptr != nullptr){
 		Node* next = ptr->next;
 		delete ptr;
 		ptr = next; 
@@ -17,7 +17,7 @@ DoublyLinkedList<T>::~DoublyLinkedList() {
 template<typename T>
 std::size_t DoublyLinkedList<T>::get_size() const noexcept {
 	std::size_t size = 0;
-	for (Node* ptr = begin; ptr != NULL; ptr = ptr->next){
+	for (Node* ptr = begin; ptr != nullptr; ptr = ptr->next){
 		++size;
 	}
 	return size;
@@ -25,7 +25,7 @@ std::size_t DoublyLinkedList<T>::get_size() const noexcept {
 
 template<typename T>
 bool DoublyLinkedList<T>::has_item(const T& value) const noexcept {
-	for (Node* ptr = begin; ptr != NULL; ptr = ptr->next){
+	for (Node* ptr = begin; ptr != nullptr; ptr = ptr->next){
 		if (ptr->value == value){
 			return true;
 		}
@@ -35,7 +35,7 @@ bool DoublyLinkedList<T>::has_item(const T& value) const noexcept {
 
 template<typename T>
 void DoublyLinkedList<T>::print() const noexcept {
-	for (Node* ptr = begin; ptr != NULL; ptr = ptr->next){
+	for (Node* ptr = begin; ptr != nullptr; ptr = ptr->next){
 		std::cout << ptr->value << " ";
 	}
 	std::cout << std::endl;
@@ -44,11 +44,13 @@ void DoublyLinkedList<T>::print() const noexcept {
 template<typename T>
 void DoublyLinkedList<T>::push_back(const T& value) {
 	Node* ptr = new Node(value);
-	ptr->prev = end;                                //prev указывает на предыдущий объект
-	if (end != NULL){
-		end->next = ptr;                           //next ведет на добавляемый объект
+	//prev указывает на предыдущий объект
+	ptr->prev = end;
+	//next ведет на добавляемый объект
+	if (end != nullptr){
+		end->next = ptr;                   
 	}
-	if (begin  == NULL){
+	if (begin  == nullptr){
 		begin = ptr;
 	}
 	end = ptr;
@@ -56,28 +58,50 @@ void DoublyLinkedList<T>::push_back(const T& value) {
 
 template<typename T>
 bool DoublyLinkedList<T>::remove_first(const T& value) noexcept {
-	if (begin != NULL){
-		Node* ptr = begin;          
-		begin = ptr->next;     
-		delete ptr;                                 
-		return true;
+	for (Node* ptr = begin; ptr != nullptr; ptr = ptr->next){
+		if (ptr->value == value){
+			//если значение в начале
+			if (ptr == begin){
+				begin = ptr->next;
+			}
+			else{
+				ptr->prev->next = ptr->next;
+			}
+			//если значение в конце
+			if (ptr == end){
+				end = ptr->prev;
+			}
+			else{
+				ptr->next->prev = ptr->prev;
+			}
+			return true;
+			break;
+		}
 	}
 	return false;
 }
 
 /*int main(){
 	DoublyLinkedList<int> vec;
-	std::cout << "Remove first: " << vec.remove_first(2) << std::endl;
+	std::cout << "Remove first 2: " << vec.remove_first(2) << std::endl;
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(2);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(5);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(4);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(7);
 	vec.print(); 
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	std::cout << "Has item 4: " << vec.has_item(4) << std::endl;
 	std::cout << "Has item 8: " << vec.has_item(8) << std::endl;
-	std::cout << "Remove first: " << vec.remove_first(2) << std::endl;
+	vec.print(); 
+	std::cout << "Remove first 10: " << vec.remove_first(10) << std::endl;
+	std::cout << "Remove first 5: " << vec.remove_first(5) << std::endl;
 	vec.print();
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	return 0;
--- 250219_CMake/projects/vector_list/src/vector.cpp
+++ 250219_CMake/projects/vector_list/src/vector.cpp
@@ -41,14 +41,24 @@ bool Vector<T>::insert(const std::size_t position, const T& value) {
 	if (position > size){
 		return false;
 	}
+
 	if (size >= capacity){
 		capacity = capacity * 2;
 	}
-	for (std::size_t i = size; i > position - 1; --i) {
-		arr[i] = arr[i - 1];
+	//создать новый массив
+	T* temp = new T[capacity];
+	//заполнить массив 
+	for (std::size_t i = 0; i < position - 1; ++i) {
+		temp[i] = arr[i];
 	}
-	arr[position - 1] = value;
+	temp[position - 1] = value;
+	for (std::size_t i = position - 1; i < size; ++i) {
+		temp[i + 1] = arr[i];
+	}
 	++size;
+	//удалить старый массив и присвоить ему всё новое 
+	delete[] arr;
+	arr = temp;
 	return true;
 }
 
@@ -63,12 +73,15 @@ void Vector<T>::print() const noexcept {
 template<typename T>
 void Vector<T>::push_back(const T& value) {
 	if (size == capacity){
-        size_t new_capacity = capacity * 2;                         //создать новый массив с capacity в 2 раза больше
+		//создать новый массив с capacity в два раза больше
+        size_t new_capacity = capacity * 2;                 
         T* result = new T[new_capacity];
-        for (decltype(size) i = 0; i < size; ++i){                  //перенести информацию из старого вектора в новый
+		//перенести информацию из старого массива в новый
+        for (std::size_t i = 0; i < size; ++i){
             result[i] = arr[i];
 		}
-        delete[] arr;                                               //удалить старый вектор и присвоить ему всё новое
+		//удалить старый массив и присвоить ему всё новое 
+        delete[] arr; 
         arr = result;
         capacity = new_capacity;
     }
@@ -77,39 +90,43 @@ void Vector<T>::push_back(const T& value) {
 
 template<typename T>
 bool Vector<T>::remove_first(const T& value) {
-	if (size != 0){
-        size_t new_size = size - 1;                         
-        T* result = new T[new_size];
-        for (decltype(size) i = 1; i < size; ++i){                  
-        	result[i-1] = arr[i];
+	for (std::size_t i = 0; i < size; ++i){
+		if (arr[i] == value){
+			for (std::size_t j = i; j < size - 1; ++j){
+				arr[j] = arr[j + 1];
+			}
+			--size;
+			return true;
+			break;
 		}
-        delete[] arr;      
-        arr = result;
-		size = new_size;
-		if (capacity > START_CAPACITY){
-			capacity = capacity - 1;
-		}
-		return true;
 	}
 	return false;
 }
 
 /*int main(){
 	Vector<int> vec;
-	std::cout << "Remove first: " << vec.remove_first(2) << std::endl;
+	std::cout << "Remove first 2: " << vec.remove_first(2) << std::endl;
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(2);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(5);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(4);
+	vec.print(); 
+	std::cout << "Size: " << vec.get_size() << std::endl;
 	vec.push_back(1);
 	vec.print(); 
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	std::cout << "Has item 4: " << vec.has_item(4) << std::endl;
 	std::cout << "Has item 8: " << vec.has_item(8) << std::endl;
-	std::cout << "Position 5 value 7: " << vec.insert(6, 7) << std::endl;
-	std::cout << "Position 3 value 10: " << vec.insert(3, 10) << std::endl;
+	std::cout << "Position 6 value 7: " << vec.insert(6, 7) << std::endl;
+	std::cout << "Position 3 value 10: " << vec.insert(3, 10) << std::endl;;
 	vec.print();
-	std::cout << "Remove first: " << vec.remove_first(2) << std::endl;
+	std::cout << "Size: " << vec.get_size() << std::endl;
+	std::cout << "Remove first 9: " << vec.remove_first(9) << std::endl;
+	std::cout << "Remove first 5: " << vec.remove_first(5) << std::endl;
 	vec.print();
 	std::cout << "Size: " << vec.get_size() << std::endl;
 	return 0;
